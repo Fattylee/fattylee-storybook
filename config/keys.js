6c1921109813b7f3507175eba3831f3c4db7b55b
Fatai Balogun <fattylee.remod@gmail.com>
2019-09-11 10:37:28 +0100

    fix config

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
 if(process.env.NODE_ENV === 'production') {
-  require('dotenv').config(); 
   module.exports = require('./prod');
 }
 else if (process.env.NODE_ENV === 'staging') { 
-  require('dotenv').config({path: '.env.stag'}); 
   module.exports = require('./stag');
 }
 else {
